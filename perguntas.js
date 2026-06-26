bca921b6d1225a32af1de3e8bebee587d8c9ed78
maryzx1 <mariany.baroni@escola.pr.gov.br>
2026-06-26 17:56:03 -0300

    flashcard

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,24 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que é uma variável na programação',
+    ' É um espaço na memória do computador usado para armazenar e organizar dados (como números ou textos) que podem ser alterados durante a execução do programa.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é o maior país do mundo em extensão territorial?',
+    'Russia '
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Qual estrutura de repetição é usada para executar um bloco de código enquanto uma condição for verdadeira??',
+    'O laço while.'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como o conceito de viés de retrospectiva influencia nossa percepção dos eventos históricos?',
+    'O viés de retrospectiva (ou hindsight bias) faz com que eventos passados pareçam muito mais previsíveis e inevitáveis do que realmente eram antes de acontecerem. Quando olhamos para a história sabendo o resultado final, temos a tendência de simplificar a narrativa, ignorando as incertezas, o caos e as outras alternativas que as pessoas daquela época enfrentavam.'
 )
 
